aad1b93ba7bce1f6c6535de1a3925ef7a6952913
fawad57 <fawadhumayun96@gmail.com>
2024-05-14 17:30:17 +0500

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,59 @@
+/* fonts */
+export const FontFamily = {
+  interMediumItalic: "Inter_medium_italic",
+  interExtralightItalic: "Inter_extralight_italic",
+  interBold: "Inter_bold",
+  interMedium: "Inter_medium",
+  interSemibold: "Inter_semibold",
+  montagaRegular: "Montaga_regular",
+  inikaRegular: "Inika_regular",
+  interBoldItalic: "Inter_bold_italic",
+  interRegularItalic: "Inter_regular_italic",
+  interSemiboldItalic: "Inter_semibold_italic",
+  bodoniModaRegular: "Bodoni Moda_regular",
+  interRegular: "Inter_regular",
+  interExtrabold: "Inter_extrabold",
+};
+/* font sizes */
+export const FontSize = {
+  size_2xs: 11,
+  size_xs: 12,
+  size_mini: 15,
+  size_8xl_4: 27,
+  size_6xl: 25,
+  size_xl_9: 21,
+  size_sm: 14,
+  size_smi: 13,
+  size_mid_1: 17,
+  size_8xl: 27,
+  size_xl: 20,
+  size_4xl: 23,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  black: "#000",
+  gray_200: "#828080",
+  gray_100: "#121111",
+  gray_300: "rgba(0, 0, 0, 0.1)",
+  steelblue_100: "#438cd0",
+  steelblue_200: "#5b7fa0",
+  steelblue_300: "rgba(84, 114, 158, 0.86)",
+  silver: "#afcaca",
+  darkslategray: "#454343",
+  gainsboro_100: "#d9d9d9",
+  dimgray_100: "#676767",
+  darkgray: "#afacac",
+  whitesmoke: "#efe7e7",
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_5xs: 8,
+  br_11xl: 30,
+  br_mini: 15,
+  br_7xs: 6,
+  br_lg: 18,
+  br_7xl_5: 27,
+  br_3xl: 22,
+};
